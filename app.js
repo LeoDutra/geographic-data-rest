41a94788014ea71efb5a02ce97d9bfdc3c92e0da
Leo Dutra <leodutra.br@gmail.com>
2015-12-08 12:10:12 -0200

    Swagger Ui middleware setup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var SwaggerRestify = require('swagger-restify-mw');
+var SwaggerUi = require('swagger/node_modules/swagger-tools/middleware/swagger-ui');
 var restify = require('restify');
 var app = restify.createServer();
 
@@ -10,15 +11,19 @@ var config = {
   appRoot: __dirname // required config
 };
 
+
 SwaggerRestify.create(config, function(err, swaggerRestify) {
   if (err) { throw err; }
+
+
+  app.use(swaggerRestify.runner.swaggerTools.swaggerUi()); // MUST BE BEFORE APP REGISTER
 
   swaggerRestify.register(app);
 
   var port = process.env.PORT || 10010;
   app.listen(port);
 
-  if (swaggerRestify.runner.swagger.paths['/country']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + 'country/byShortISO/BR');
+  if (swaggerRestify.runner.swagger.paths['/countries']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + 'countries');
   }
 });
