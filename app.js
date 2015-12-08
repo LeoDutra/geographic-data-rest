37a781074ff72a2435b46cd19637e0986110ad38
Leo Dutra <leodutra.br@gmail.com>
2015-12-08 12:34:07 -0200

    Swagger Ui middleware setup

--- app.js
+++ app.js
@@ -11,12 +11,18 @@ var config = {
   appRoot: __dirname // required config
 };
 
+// app.get('u', restify.serveStatic({
+//     directory: '/public/docs/'
+// }));
 
+// console.log(__dirname + 'public/docs')
 SwaggerRestify.create(config, function(err, swaggerRestify) {
   if (err) { throw err; }
 
 
-  app.use(swaggerRestify.runner.swaggerTools.swaggerUi()); // MUST BE BEFORE APP REGISTER
+  app.use(swaggerRestify.runner.swaggerTools.swaggerUi({
+  	url:"http://leostuff.ddns.net:10010/api-docs"
+  })); // MUST BE BEFORE APP REGISTER
 
   swaggerRestify.register(app);
 
@@ -24,6 +30,6 @@ SwaggerRestify.create(config, function(err, swaggerRestify) {
   app.listen(port);
 
   if (swaggerRestify.runner.swagger.paths['/countries']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + 'countries');
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/countries');
   }
 });
